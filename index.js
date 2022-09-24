baf02a7825549d694736293aefd670f3cbaa9d4a
raygdev <101662915+raygdev@users.noreply.github.com>
2022-09-23 23:20:48 -0400

    Reformat index.js

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
-const { MongoClient } = require('mongodb')
-require('dotenv').config()
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
 
-async function main(){
-    const uri = process.env.MONGOURI 
+async function main() {
+  const uri = process.env.MONGOURI;
 
-    const client = new MongoClient(uri)
+  const client = new MongoClient(uri);
 
-   try { await client.connect()
+  try {
+    await client.connect();
 
-    await listDatabases(client)
-} catch (e) {
-    console.error(e)
-}
-finally {
-    await client.close()
-}
-
-}
-
-async function listDatabases(client){
-    databasesList = await client.db().admin().listDatabases();
-
-    console.log("Databases:")
-
-    databasesList.databases.forEach(db => {
-        console.log(` - ${db.name}`)
-    });
+    await listDatabases(client);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
 }
 
-main().catch(console.error)
+async function listDatabases(client) {
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+
+  databasesList.databases.forEach((db) => {
+    console.log(` - ${db.name}`);
+  });
+}
+
+main().catch(console.error);
