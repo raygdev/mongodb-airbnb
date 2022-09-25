73d82f55d60ccc01145e07027868a784798690d5
raygdev <101662915+raygdev@users.noreply.github.com>
2022-09-25 16:27:27 -0400

    Call resolve to kill process
    called resolve to kill process after closing
    changeStream
    
    imported pipeline to use for changeStream

--- changeStream.js
+++ changeStream.js
@@ -1,40 +1,50 @@
-const { MongoClient } = require('mongodb')
-require('dotenv').config()
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const pipelines = require("./pipelines.js");
 
 async function main() {
-    const uri = process.env.MONGOURI
-    const client = new MongoClient(uri)
+  const uri = process.env.MONGOURI;
+  const client = new MongoClient(uri);
 
-    try {
-        await client.connect()
-        await monitorListingsUsingEventEmitter(client)
-    }
-    catch (e) {
-        console.error(e)
-    }
-    finally {
-        client.close()
-    }
+  try {
+    await client.connect();
+    await monitorListingsUsingEventEmitter(
+      client,
+      30000,
+      pipelines.testPipeline
+    );
+  } catch (e) {
+    console.error(e);
+  } finally {
+    client.close();
+  }
 }
 
 function closeChangeStream(timeInMs = 60000, changeStream) {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            console.log('Closing the change stream')
-            changeStream.close()
-        }, timeInMs)
-    })
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("Closing the change stream");
+      changeStream.close();
+      resolve();
+    }, timeInMs);
+  });
 }
 
-async function monitorListingsUsingEventEmitter(client, timeInMs = 60000, pipeline = []) {
-    const collection = client.db('sample_airbnb').collection('listingsAndReviews')
+async function monitorListingsUsingEventEmitter(
+  client,
+  timeInMs = 60000,
+  pipeline = []
+) {
+  const collection = client
+    .db("sample_airbnb")
+    .collection("listingsAndReviews");
 
-    const changeStream = collection.watch(pipeline)
+  const changeStream = collection.watch(pipeline);
 
-    changeStream.on('change', (next) => {
-        console.log(next)
-    })
-    await closeChangeStream(timeInMs, changeStream)
+  changeStream.on("change", (next) => {
+    console.log(next);
+  });
+  await closeChangeStream(timeInMs, changeStream);
 }
 
-main().catch(console.error)
+main().catch(console.error);
