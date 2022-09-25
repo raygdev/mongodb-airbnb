c40c98b67fb59f3319a728707aa5d19cfa7d58ee
raygdev <101662915+raygdev@users.noreply.github.com>
2022-09-25 14:56:31 -0400

    Add crud operations for testing change stream
    added crud operations to create and update listings.
    implimented function inside of main
    
    moved listings inside of try block after db connect
    so it wouldn't throw an error

--- changeStreamsTestData.js
+++ changeStreamsTestData.js
@@ -1,57 +1,89 @@
-const { MongoClient } = require('mongodb')
-require('dotenv').config()
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
 
-async function main(){
-    const uri = process.env.MONGOURI
-    const client = new MongoClient(uri)
+async function main() {
+  const uri = process.env.MONGOURI;
+  const client = new MongoClient(uri);
 
-    const operaHouse = await createListing(client,{
-        name: 'Opera House Views',
-        summary: "Beautiful apartment with the views of the iconic Sydney Opera House",
-        property_type: 'Apartment',
-        bedrooms: 1,
-        bathrooms: 1,
-        beds: 1,
-        address:{
-            market: 'Sydney',
-            country: "Australia"
-        }
-    })
+  try {
+    await client.connect();
 
-    const privateRoomInLondon = await createListing(client,{
-        name: 'Private room in London',
-        property_type: "Apartment",
-        bedrooms: 1,
-        bathrooms: 1
-    })
+    const operaHouseViews = await createListing(client, {
+      name: "Opera House Views",
+      summary:
+        "Beautiful apartment with the views of the iconic Sydney Opera House",
+      property_type: "Apartment",
+      bedrooms: 1,
+      bathrooms: 1,
+      beds: 1,
+      address: {
+        market: "Sydney",
+        country: "Australia",
+      },
+    });
+
+    const privateRoomInLondon = await createListing(client, {
+      name: "Private room in London",
+      property_type: "Apartment",
+      bedrooms: 1,
+      bathrooms: 1,
+    });
 
     const beautifulBeachHouse = await createListing(client, {
-        name: "Beautiful Beach House",
-        summary: "Enjoy relaxed beach living in this house with a private beach",
-        bedrooms: 4,
-        bathrooms: 2.5,
-        beds: 7,
-        last_review: new Date()
-    })
+      name: "Beautiful Beach House",
+      summary: "Enjoy relaxed beach living in this house with a private beach",
+      bedrooms: 4,
+      bathrooms: 2.5,
+      beds: 7,
+      last_review: new Date(),
+    });
 
-    try {
-        await client.connect()
-    }
-    catch (e) {
-        console.error(e)
-    }
-    finally {
-        client.close()
-    }
+    await updateListing(client, operaHouseViews, { beds: 2 });
+
+    await updateListing(client, beautifulBeachHouse, {
+      address: {
+        market: "Sydney",
+        country: "Australia",
+      },
+    });
+  } catch (e) {
+    console.error(e);
+  } finally {
+    client.close();
+  }
 }
 
+/**
+ * Create a new Airbnb listing
+ * @param {MongoClient} client a mongo client that is connected to a cluster
+ * @param {Object} newListing the new listing to be added
+ * @returns {String} the id of the new listing
+ */
 
 async function createListing(client, newListing) {
-    const result = await client.db('sample_airbnb')
-            .collection('listingsAndReviews')
-            .insertOne(newListing)
-    console.log(`New listing created with the following id: ${result.insertedId}`)
-    return result.insertedId
+  const result = await client
+    .db("sample_airbnb")
+    .collection("listingsAndReviews")
+    .insertOne(newListing);
+  console.log(
+    `New listing created with the following id: ${result.insertedId}`
+  );
+  return result.insertedId;
 }
 
-main().catch(console.error)
+/**
+ * Update an Airbnb lisiting
+ * @param {MongoClient} client A MongoClient that is connected to a cluster
+ * @param {String} listingId the id of the listing you want to update
+ * @param {Object} updatedListing an object containing all the properties to be updated for the given listing
+ */
+
+async function updateListing(client, listingId, updatedListing) {
+  const result = await client
+    .db("sample_airbnb")
+    .collection("listingsAndReviews")
+    .updateOne({ _id: listingId }, { $set: updatedListing });
+  console.log(`${result.matchedCount} document(s) matched the query criteria`);
+  console.log(`${result.modifiedCount} document(s) was/were updated`);
+}
+main().catch(console.error);
